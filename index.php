868a1ff815931df7603c09396464b6b746a241bc
Rene Teixeira Francisco <renetf77@gmail.com>
2017-03-02 00:49:13 -0300

    37

--- index.php
+++ index.php
@@ -7,7 +7,7 @@ and open the template in the editor.
 <html>
     <head>
         <meta charset="UTF-8">
-        <title>infogate35</title>
+        <title>infogate37</title>
     </head>
     <body>
         <?php
